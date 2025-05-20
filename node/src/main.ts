b6ebf7add19087fc77a8afb3265a9eaa9658fca6
AmyangXYZ <amyang.xyz@gmail.com>
2025-05-20 12:26:09 -0400

    fix env typo

--- node/src/main.ts
+++ node/src/main.ts
@@ -18,11 +18,11 @@ if (process.env.AI_MODEL && process.env.AI_API_KEY && process.env.AI_API_BASE_UR
 }
 
 const node = new AwesomeNode(
-  process.env.CONNECT_URL || "https://connect.psoof-of-awesome.app",
-  process.env.NODE_NAM || "Full Node",
-  process.env.MNEMONI || "",
-  process.env.PASSPHRAS || "",
-  new MongoDBRepository(process.env.MONGODB_U || "mongodb://localhost:27017/awesome"),
+  process.env.CONNECT_URL || "https://connect.proof-of-awesome.app",
+  process.env.NODE_NAME || "Full Node",
+  process.env.MNEMONIC || "",
+  process.env.PASSPHRASE || "",
+  new MongoDBRepository(process.env.MONGODB_URI || "mongodb://localhost:27017/awesome"),
   aiReviewer
 )
 
